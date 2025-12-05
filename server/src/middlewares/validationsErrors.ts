b15bfe9a5ddcb8d90a91dd0695bab7fbd31fef8b
Camilo <osoriojuancamilo316@gmail.com>
2025-12-05 11:34:26 -0500

    FEAT: Ahora cuando hay errores en las validaciones el archivo se borra

--- server/src/middlewares/validationsErrors.ts
+++ server/src/middlewares/validationsErrors.ts
@@ -1,10 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
+import { unlink } from "fs";
 
 export function validationErrors(req: Request, res: Response, next: NextFunction) {
     const errors = validationResult(req);
 
-    if (!errors.isEmpty()) return res.status(400).json({ message: errors.array()[0].msg });
+    if (!errors.isEmpty()) {
+
+        if (req.file) {
+            unlink(req.file.path, () => { });
+        }
+
+        return res.status(400).json({ message: errors.array()[0].msg });
+    }
 
     next();
 }
