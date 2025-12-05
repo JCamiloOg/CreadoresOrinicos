2f380d13a05fc184aada6615e32ce53404c3b2bf
Camilo <osoriojuancamilo316@gmail.com>
2025-12-05 11:40:49 -0500

    FEAT: Se agregan las rutas de los modulos

--- server/src/routes/admin/admin.routes.ts
+++ server/src/routes/admin/admin.routes.ts
@@ -1,12 +1,20 @@
 import { login } from "@/controllers/admin/users.controller";
 import { Router } from "express";
-import usesrRouter from "./userRoutes/users.routes";
 import { validateLogin } from "@/middlewares/validations/validateUser";
 import { validationErrors } from "@/middlewares/validationsErrors";
 import { checkAuthAdmin } from "@/middlewares/authAdmin";
+
+import usesrRouter from "./userRoutes/users.routes";
+import glossaryRouter from "./glossaryRoutes/glossary.routes";
+import eventsRouter from "./eventsRoutes/events.routes";
+import articleRouter from "./articlesRoutes/articles.routes";
+
 const router = Router();
 
 router.post("/login", validateLogin, validationErrors, login);
 router.use("/users", checkAuthAdmin, usesrRouter);
+router.use("/glossary", checkAuthAdmin, glossaryRouter);
+router.use("/events", checkAuthAdmin, eventsRouter);
+router.use("/articles", checkAuthAdmin, articleRouter);
 
 export default router;
