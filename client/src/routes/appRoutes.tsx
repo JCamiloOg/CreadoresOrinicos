f190dad75ca9f78c0414e5dc5d3fda56bbf4fd9a
Camilo <osoriojuancamilo315@gmail.com>
2025-12-21 21:55:27 -0500

    FEAT: rutas del login y del blog para el admin

--- client/src/routes/appRoutes.tsx
+++ client/src/routes/appRoutes.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router";
 import Index from "../pages/index";
 import SocialNetworks from "@/pages/socialNetworks";
+import Login from "@/pages/admin/login";
+import BlogAdmin from "@/pages/admin/blog";
 
 export default function AppRoutes() {
     return (
@@ -8,6 +10,10 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/contact" element={<SocialNetworks />} />
+
+                {/* Admin */}
+                <Route path="/admin/login" element={<Login />} />
+                <Route path="/admin/blog" element={<BlogAdmin />} />
             </Routes>
         </>
     );
