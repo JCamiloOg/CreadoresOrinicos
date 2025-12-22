5d96032377310757e830f40fff8924b931d86332
Camilo <osoriojuancamilo315@gmail.com>
2025-12-21 21:53:46 -0500

    FEAT: Hook que se usa al cambiar el idioma

---
+++ client/src/hooks/useChangeLanguage.ts
@@ -0,0 +1,20 @@
+import i18n from "@/config/i18n";
+import type { Dispatch, SetStateAction } from "react";
+
+export default function onChangeLanguage(setValue: Dispatch<SetStateAction<boolean>>, action?: () => void) {
+    i18n.on("languageChanging", () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+        setValue(true);
+    });
+    i18n.on("languageChanged", () => {
+        const timer = setTimeout(() => {
+            if (action) action();
+            setValue(false);
+        }, 500);
+        return () => {
+            clearTimeout(timer);
+        };
+    });
+
+
+}
