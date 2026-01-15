d3f54ee9a596b9f0c9967893b8cf6217eb3b5632
Camilo <osoriojuancamilo315@gmail.com>
2026-01-14 22:04:11 -0500

    FEAT: Se añade la configuracion de AOS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
 import { Toaster } from "sonner";
 import AppRoutes from "./routes/appRoutes";
 import "@/config/i18n";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      once: false,
+      mirror: true,
+      offset: 120
+    });
+  }, []);
+
   return (
     <>
       <AppRoutes />
