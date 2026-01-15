13fbd2cf42b87c8c2d00a250ac520eee72241ea0
Camilo <osoriojuancamilo315@gmail.com>
2026-01-14 21:56:33 -0500

    FEAT: Ahora se guarda el idioma en localStorage y lo carga del mismo.

--- client/src/config/i18n.ts
+++ client/src/config/i18n.ts
@@ -3,13 +3,22 @@ import { initReactI18next } from "react-i18next";
 import en from "@/locales/en.json";
 import es from "@/locales/es.json";
 
+const storedLang = localStorage.getItem("lang");
+
+const initialLang = storedLang || navigator.language.split("-")[0] || "es";
+
+
 i18n
     .use(initReactI18next)
     .init({
         resources: { en: { translation: en }, es: { translation: es } },
-        lng: "es",
+        lng: initialLang,
         fallbackLng: "es",
         interpolation: { escapeValue: false },
+        supportedLngs: ["es", "en"],
+        nonExplicitSupportedLngs: true,
+        load: "languageOnly",
     });
+
 
 export default i18n;
