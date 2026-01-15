52fb243fedef100fae7abfa1fc6f9ebc4717cccd
Camilo <osoriojuancamilo315@gmail.com>
2026-01-14 22:04:51 -0500

    Se añaden todas las rutas

--- client/src/routes/appRoutes.tsx
+++ client/src/routes/appRoutes.tsx
@@ -5,19 +5,37 @@ import Login from "@/pages/admin/login";
 import BlogAdmin from "@/pages/admin/blog";
 import EventsAdmin from "@/pages/admin/events";
 import GlossaryAdmin from "@/pages/admin/glossary";
+import Services from "@/pages/services";
+import Service from "@/pages/service";
+import NotFound from "@/components/notFound/not-found";
+import AboutUs from "@/pages/about-us";
+import Blog from "@/pages/blog";
+import Events from "@/pages/events";
+import UsersAdmin from "@/pages/admin/users";
+import Glossary from "@/pages/glossary";
 
 export default function AppRoutes() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Index />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/events" element={<Events />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/services/:service" element={<Service />} />
                 <Route path="/contact" element={<SocialNetworks />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/glossary" element={<Glossary />} />
+
 
                 {/* Admin */}
                 <Route path="/admin/login" element={<Login />} />
                 <Route path="/admin/blog" element={<BlogAdmin />} />
                 <Route path="/admin/events" element={<EventsAdmin />} />
                 <Route path="/admin/glossary" element={<GlossaryAdmin />} />
+                <Route path="/admin/users" element={<UsersAdmin />} />
+
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     );
