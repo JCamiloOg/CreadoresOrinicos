d86207ea6ac945b751392dac84e82d9baa459dcf
Camilo <osoriojuancamilo315@gmail.com>
2026-01-14 22:02:51 -0500

    FEAT: ahora el hook puede recibir un breakPoint de pantalla

--- client/src/hooks/use-mobile.ts
+++ client/src/hooks/use-mobile.ts
@@ -1,8 +1,7 @@
 import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768;
 
-export function useIsMobile() {
+export function useIsMobile(MOBILE_BREAKPOINT = 768) {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
@@ -13,7 +12,7 @@ export function useIsMobile() {
     mql.addEventListener("change", onChange);
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     return () => mql.removeEventListener("change", onChange);
-  }, []);
+  }, [MOBILE_BREAKPOINT]);
 
   return !!isMobile;
 }
