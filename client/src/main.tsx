d8bac1d7f53c0bc21c4dc6de32aed70ffae0acea
Camilo <166083316+JCamiloOg@users.noreply.github.com>
2026-05-05 11:41:58 -0500

    StricMode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,13 @@ import "./styles/background.css";
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
 import ScrollToTop from './hooks/scrollToTop.ts';
+import { StrictMode } from "react";
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>,
+  </StrictMode>
 );
